5934e58a699ae6598174269348a2dc20bc1eed72
priyaa74 <priyavarshney070497@gmail.com>
2022-11-30 20:12:32 +0530

    seller page

---
+++ Components/Registration.js
@@ -0,0 +1,23 @@
+let structureBox = (modalId, modalLabelId, modalTitle, inputType, inputID, placeholder, submitBtnID, nextModalID, btnName) => {
+  let modal;
+  modal = `<div id="${modalId}" tabindex="-1">
+            <h2 class="modal-title" id="${modalLabelId}">${modalTitle}</h2>
+            </div>
+            <div class="d-flex">
+            
+            </div>
+            
+              <div id="input">
+              ${inputType === 'phoneNumber' ? '<div class="col-2"><button id="cCode">+91</button></div>' : ''}
+                
+                  <input type="number" id="${inputID}" placeholder="${placeholder}">
+              
+            </div>
+            <div class="d-flex my-3">
+              <button data-bs-dismiss="modal" data-bs-target="#${nextModalID ? nextModalID : modalId}" data-bs-toggle="modal" id="${submitBtnID}">${btnName}</button>
+            </div>
+    </div>`;
+  return modal;
+}
+
+export default structureBox;
---
+++ Scripts/index.js
@@ -0,0 +1,48 @@
+
+// Creating registration components
+import structureBox from "../Components/Registration.js";
+
+let generateOTP = (otpVerify) => {
+  if (otpVerify.length > 0) {
+    otpVerify = '';
+  }
+  for (let i = 0; i < 4; i++) {
+    let num = Math.round(Math.random() * 10);
+    if (num === 10) {
+      num--;
+    }
+    otpVerify += num;
+  }
+  return otpVerify;
+}
+// console.log(createModal);
+let loginModalSection = document.getElementById('loginModalSection');
+loginModalSection.innerHTML = structureBox('loginModal', 'loginModalLabel', 'Free Registration/Sign In', 'phoneNumber', 'mobileInput', 'Enter Your Mobile Number', 'loginToOTPBtn', 'otpModal','Start Selling <i class="fa-solid fa-arrow-right"></i>');
+let loginToOTPBtn = document.getElementById('loginToOTPBtn');
+let contact;
+let otpVerify = ''
+loginToOTPBtn.addEventListener('click', () => {
+  // console.log('Hi');
+  contact = document.getElementById('mobileInput').value;
+  // console.log(contact);
+  otpVerify = generateOTP(otpVerify);
+  console.log('otpVerify:', otpVerify);
+  alert("otp:"+otpVerify);
+  let otpModalSection = document.getElementById('otpModalSection');
+  otpModalSection.style.display="block";
+otpModalSection.innerHTML = structureBox('otpModal', 'otpModalLabel', `Enter the 4 digit One Time Password (OTP) sent to your Mobile Number`, 'otp', 'otpInput', 'X-X-X-X', 'otpModalButton', '','Verify OTP');
+let otpModalButton = document.getElementById('otpModalButton');
+otpModalButton.addEventListener('click', () => {
+  let otp = document.getElementById('otpInput').value;
+  // console.log(otp);
+  if (otp === otpVerify) {
+   alert("OTP verified successfully");
+
+  }
+  else {
+    alert('Invalid OTP');
+  }
+  otpModalSection.style.display="none";
+});
+});
+
